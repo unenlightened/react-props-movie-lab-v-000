4e9695cddbd3811811149658762cd8d7eed4bf4d
Chely Ho <adept_inept@yahoo.com>
2018-09-22 16:21:54 +0000

    automatically backed up by learn

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -32,7 +32,7 @@ export default class MovieCard extends Component {
   render() {
     return (
       <div className="movie-card">
-        <CardFront poster={this.props.poster} />
+        <CardFront poster={posterMap[this.props.poster]} />
         <CardBack title={this.props.title} IMDBRating={this.props.IMDBRating} genres={this.props.genres} />
       </div>
     )
